2bd799252602e490c9f31fbcc5b34a80ddcb00d0
ADI GUPTA <vlogzx08@gmail.com>
2024-05-31 20:08:24 +0530

    Update start.py

--- OXYBOT/modules/start.py
+++ OXYBOT/modules/start.py
@@ -35,7 +35,7 @@ async def start(event):
         TEXT = f"**ʜᴇʏ [{event.sender.first_name}](tg://user?id={event.sender.id}),\n\nɪ ᴀᴍ [{bot_name}](tg://user?id={bot_id})**\n"
         await event.client.send_file(
             event.chat_id,
-            "https://telegra.ph/file/b6b1b2d4450c9a5917827.jpg",
+            "https://telegra.ph/file/3bc1f1081832a07a59320.jpg",
             caption=TEXT,
             buttons=START_BUTTON
         )
